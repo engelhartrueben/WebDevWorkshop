7c352b330300373430240ad50e75c006d7e204ea
Rueben Engelhart <engelhartrueben@gmail.com>
2025-02-13 20:00:24 -0500

    proof of concept with some legitimacy. Can currently send form data, however, it is messy

--- server/app/main.py
+++ server/app/main.py
@@ -1,30 +1,55 @@
 # main.py
 
-from typing import Union, Annotated
-from fastapi import FastAPI, Header, Path
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
+origin = [
+        "http://localhost:8000",
+        "http://0.0.0.0:8000",
+]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'], # this is a touch danger
+        allow_methods=['*'],
+        allow_headers=['*']
+)
+
+class Test(BaseModel):
     name: str
-    description: str | None = None
     price: float
-    tax: float | None = None
+    count: int
+
+class Simple(BaseModel):
+    item: int
+
+class Form(BaseModel):
+    fname: str | None = None
+    lname: str | None = None
+    email: str | None = None
+    sid: str | None = None
+    year: str | None = None
+    event_1: str | None = None
+    event_2: str | None = None
+    event_3: str | None = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/sub/")
-def addSubmission(
-        item_id: Annotated[int, Path(title="title", ge=0, le=100)],
-        q: str | None = None,
-        item: Item | None = None,
-        ):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q":q})
-    if item:
-        results.update({"item":item})
-    return results
+@app.post("/test/")
+def test(item: Test):
+    return {"congrats": "True"}
+
+@app.post("/again/")
+def again(item: Simple) -> int:
+    return item.item ** 2
+
+@app.post("/submit/")
+def submitForm(form: Form) -> None:
+    for things in form:
+        print(things)
+
