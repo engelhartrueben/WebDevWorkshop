f8138cfc317f156f533eadd4ee551902c7ef6f15
Rueben Engelhart <engelhartrueben@gmail.com>
2025-02-13 20:00:00 -0500

    proof of concept. currently works with current fastapi

--- server/post.js
+++ server/post.js
@@ -1,23 +1,29 @@
 
-const IP = "http://localhost:8989/";
+const IP = "http://0.0.0.0:8000/";
 
-fetch(IP)
-	.then((response) => response.json())
-	.then((json) => console.log(json));
-
-fetch(IP + "sub/", {
+fetch(IP + "test/", {
 	method: "POST",
-	body: JSON.stringify({
-		"item": {
-    			"name": "Foo",
-    			"description": "The pretender",
-			"price": 42.0,
-			"tax": 3.2
-		}
-	}),
 	headers: {
-		"Content-Type": "application/json"
-	}
+		'Accept': 'application/json',
+		'Content-type': 'application/json'
+	},
+	body: JSON.stringify({
+		"name": "pizza",
+		"price": 7.7,
+		"count": 8
+	}),
 })
 	.then((response) => response.json())
 	.then((json) => console.log(json));
+
+fetch(IP + "again/", {
+	method: "POST",
+	headers: {
+		'Accept': 'application/json',
+		'Content-type': 'application/json'
+	},
+	body: JSON.stringify({ "item": 5 }),
+})
+	.then(r => r.json())
+	.then(j => console.log(j));
+
