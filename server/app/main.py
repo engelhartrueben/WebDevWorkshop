d396eb7600a15f180cd0e2d556add1b5d324abc7
Rueben Engelhart <engelhartrueben@gmail.com>
2025-02-15 09:47:22 -0500

    comment out mariadb option. don't need to go this route for something so simple. added in logic to pring to a csv instead.

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,20 @@
 # main.py
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware 
 from pydantic import BaseModel
 
+import os
+import mariadb
+
+from datetime import datetime
+
+from dotenv import dotenv_values
+
 app = FastAPI()
+
+# load .env found in this directory
+config = dotenv_values(f"{os.getcwd()}/.env")
 
 # There is a way to do this with regex...
 # might be less annoying to just allow all origins
@@ -35,7 +45,44 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/submit/")
-def submitForm(form: Form) -> None:
-    for things in form:
-        print(things)
+def submitForm(form: Form, requests: Request):
+    # conn = connectToMariaDb()
+    # if connection error, simply return the error
+    # NOTE we do not actually return the connection error with
+    # mariadb. Probably a bad security thing goin on there
+    # if (conn.ERROR):
+    #     return conn.ERROR
 
+    file = open(f"{os.getcwd()}/{config["OUTPUT_FILE_NAME"]}", "a+")
+    file.write(f"{str(requests.client)}, ")
+    file.write(datetime.now().strftime("%m/%d/%Y, %H:%M.%S, "))
+    for k,v in form.model_dump().items():
+        if (k in "event"):
+            file.write(str(v))
+        else:
+            file.write(f"{str(v)}, ")
+    file.write("\n")
+        
+
+    print(requests.client)
+
+# holding off on this.. probably just going to place everything
+# in a plain text file for simplicity sake. 
+'''
+def connectToMariaDb():
+    # this may cause an error lol
+    try:
+        conn = mariadb.connect(
+                user=config["USER"],
+                database=config["DATABASE"],
+                password=config["DATABASE_PASSWORD"],
+                port=config["PORT"],
+                host=config["HOST"]
+                )
+    except mariadb.Error as e:
+        print(f"Error connected to mariadb: {e}")
+        return {"ERROR": "Unable to connect to mariadb"} 
+    return conn
+'''
+    
+                
